68523239a100f004248806ac36f797c6c32cf26d
addison <543276564@qq,com>
2018-06-04 11:49:19 +0800

    update

--- src/app/reactive/reactive.component.ts
+++ src/app/reactive/reactive.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
-
+import 'rxjs/Rx'
 @Component({
   selector: 'app-reactive',
   templateUrl: './reactive.component.html',
@@ -8,17 +8,27 @@ import { FormControl } from '@angular/forms';
 })
 export class ReactiveComponent implements OnInit {
 
-	constructor() { }
+	constructor() {
+		this.searchInput.valueChanges
+		.debounceTime(500)
+		.subscribe(
+			value => this.printInput(value)
+		)
+	}
 	  
 	searchInput:FormControl = new FormControl();
 
  	ngOnInit() {
-
-  	}
+		
+	}
+	  
 	doOnInput(event){
 		console.log(event.target.value)
 	}
 	myINPUT(event){
 		console.log(event.value)
+	}
+	printInput(value){
+		console.log(value)
 	}
 }
